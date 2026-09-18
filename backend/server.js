4e8d131e8f808a4f5dbbff54bc0c53cf4e83b5c2
Niraj Kumar Jha <nirajkrjha1212@gmail.com>
2026-09-18 17:58:49 +0530

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,172 @@
+const express= require("express")
+const Database= require("better-sqlite3")
+
+//creates instance of express , obj through wich we configure routes and middleware
+const app =express()
+
+//middleware
+app.use(express.json())
+
+//serve frontend
+app.use(express.static("../frontend"));
+
+//initialise a sqlite database
+const db = new Database("expenses.db")
+
+db.prepare(`
+    CREATE TABLE IF NOT EXISTS expenses(
+       id iNTEGER PRIMARY KEY AUTOINCREMENT,
+       amount  REAL NOT NULL,
+       description TEXT NOT NULL,
+       category TEXT NOT NULL,
+       date TEXT NOT NULL
+    )
+`).run();
+
+//Get all or filtered expenses
+app.get("/api/expenses", (req,res)=>{
+    try{
+        const{category,month} = req.query;   //.query as we look for it in the URL
+
+        let query= "SELECT * FROM expenses";  //Baseline for dynamic query
+        const conditions=[];      //Initialising conditions for the dynamic query building
+        const values={};
+
+        //Categor Filter
+        if(category && category!=="All"){
+            conditions.push("category=@category");
+            values.category= category;
+        }
+
+        //month filter
+        if(month){
+            conditions.push("date LIKE @month");
+            values.month=`${month}%`;
+        }
+
+        //If filters exists add WHERE and join the values to build the query
+        if(conditions.length>0){
+            query += " WHERE " + conditions.join(" AND "); //joins condn with 'and' in between  
+        }  //Space before and after WHERE and AND is very imp, if not the query might be invalid
+
+        //further building query to get newestdates first
+        query += " ORDER BY DATE DESC, id DESC";
+
+        const expenses= db.prepare(query).all(values);
+        res.json(expenses);
+    }catch(error){
+        console.error(error);
+        res.status(500).json({message: "Failed to load expenses"})
+
+    }
+});
+
+//POST-add expense
+app.post("/api/expenses" , (req,res)=>{
+    try{
+        const{amount,description,category,date} = req.body;  //.body as we look for it in the req being sent
+
+        //Validate that no field is empty
+        if (!amount||!description||!category||!date){
+            return res.status(400).json({message: "All fields are required"})
+        };
+
+        if (Number.isNaN(Number(amount)) || Number(amount) < 0) {  //checks if amount is a +ve num
+            return res.status(400).json({
+            message: "Amount must be a valid number"
+            });
+        }
+
+        const result =db.prepare(`
+            INSERT INTO expenses (amount, description,category,date)
+            VALUES(@amount, @description,@category,@date)
+            `).run({
+                amount:Number(amount), description,category,date
+            });
+
+            //To get the newly added expense to show up on the page
+        const expense =db.prepare(`
+            SELECT * FROM expenses WHERE id=?
+            `).get(result.lastInsertRowid);
+
+        res.status(201).json({message: "Expense added successfully", expense});
+    }catch(error){
+        console.error(error);
+        res.status(500).json({message:"Failed to add expense"});
+    }
+});
+
+//PUT Edit/Update an existing record
+app.put("/api/expenses/:id", (req,res)=>{
+    try{
+        const id=Number(req.params.id);  //Take the id from the URL, convert it into a number, and store it in id.
+        const {amount,description,category,date} = req.body;
+
+        //Validate that no field is empty
+        if (!amount||!description||!category||!date){
+            return res.status(400).json({message: "All fields are required"})
+        };
+
+        if (Number.isNaN(Number(amount)) || Number(amount) < 0) {
+            return res.status(400).json({
+            message: "Amount must be a valid number"
+            });
+        }
+
+        //Check if the expense exists
+        const existingExpense= db.prepare('SELECT * FROM expenses WHERE id =?').get(id);
+
+        if(!existingExpense){
+            return res.status(404).json({message: "Expense not found"});
+        }
+
+        //Update expense
+        db.prepare(`
+            UPDATE expenses
+            SET amount=@amount, description=@description,category=@category,date=@date
+            WHERE id=@id
+            `).run({
+                id,
+                amount: Number(amount),
+                description,category,date
+            });
+
+        const updatedExpense= db.prepare(`
+            SELECT * FROM expenses WHERE id=?
+            `).get(id);
+
+        res.json({message:"Expense Update successful",
+            expense: updatedExpense
+        });
+    }catch(error){
+        console.error(error);
+        res.status(500).json({message:"Failed to update expense"});
+    }
+});
+
+//Delete Expense
+app.delete("/api/expenses/:id", (req,res)=>{
+    try{
+        const id = Number(req.params.id);
+
+        const result = db.prepare(`
+            DELETE FROM expenses WHERE id=?
+            `).run(id);
+
+        //IF no rows are effected, means no such expense existed
+        if (result.changes===0){
+            return res.status(404).json({message:"Expense not found"})
+        };
+
+        res.json({message:"Expense deleted"})
+    }catch(error){
+        console.error(error);
+        res.status(500).json({message:"Failed to delete expense"})
+    }
+})
+
+
+//Starting the server
+app.listen(3000,()=>{
+    console.log("Server is running on port 3000");
+});
---
+++ frontend/script.js
@@ -0,0 +1,216 @@
+const form = document.querySelector("form");  //selects 1st form tag in html 
+const amount= document.querySelector("#amount");  // # used to refer to tag id
+const description= document.querySelector("#description");
+const category = document.querySelector("#category");
+const date =document.querySelector("#date");
+
+const expenseList= document.querySelector("#expense-list");
+const submitButton = document.querySelector("#submit-btn");
+
+const categoryFilter =document.querySelector("#category-filter");
+const monthFilter = document.querySelector("#month-filter");
+
+const loadingMessage =document.querySelector("#loading");
+const errorMessage = document.querySelector("#error-message");
+
+//Initialise expense id and store it when editing
+let editingExpenseId =null;
+
+//Submit handler
+form.addEventListener("submit", function(event){
+    event.preventDefault(); //prevents(default actions) refresh or redirecting of the page when butn clicked
+
+    const expense ={
+        amount : amount.value,
+        description : description.value,
+        category :category.value,
+        date : date.value
+    };
+
+    //initialise standard submit url and method post(adding epense)=>Dynamic URL/method toggle
+    let url = "http://localhost:3000/api/expenses";
+    let method ="POST";
+
+
+    //If editing use PUT method
+    if (editingExpenseId!==null){
+        url =`http://localhost:3000/api/expenses/${editingExpenseId}`;
+        method ="PUT";
+    }
+
+    fetch(url,{
+        method: method,
+        headers : {"Content-type": "application/json"},
+        body : JSON.stringify(expense)
+    })
+
+    .then(response =>{
+        if (!response.ok){
+            throw new Error("Failed to save expense");
+            
+        }
+
+        return response.json();
+    })
+
+    .then(data =>{
+        console.log(data);
+        form.reset();
+        editingExpenseId=null;
+        submitButton.textContent="Add Expense";
+        loadExpenses();
+    })
+    .catch(error=>{
+        console.error(error);
+        showError("Could not save expense.");
+    });
+});
+
+//To load expenses (Both all and filtered expenses)
+function loadExpenses() {
+    showLoading();
+    hideError();
+
+    let url = "http://localhost:3000/api/expenses"
+    const params = new URLSearchParams();
+
+    //category filter
+    if (categoryFilter.value && categoryFilter.value!=="All"){
+        params.append("category", categoryFilter.value);
+    }
+
+    //month filter
+    if (monthFilter.value){
+        params.append("month", monthFilter.value);
+    }
+
+
+    if(params.toString()){
+        url += "?" + params.toString();
+           //includes or adds the filter parameters in the url (i.e ?category=Food&date=2026-09)
+    }
+
+    fetch(url)   //DEFAULTS to GET as no method is given
+    .then(response=>{
+        if (!response.ok){
+            throw new Error("Failed to load expenses");
+        }
+        return response.json();
+    })
+
+    .then(expenses=>{
+        displayExpenses(expenses);
+        hideLoading();
+    })
+
+    .catch(error=>{
+        console.error(error);
+        hideLoading();
+        showError("Could not load expenses");
+    });
+}
+
+
+function displayExpenses(expenses) {
+    expenseList.innerHTML ="";
+
+    //Calc total
+    const total= expenses.reduce((sum,expense) =>{
+        return sum+Number(expense.amount);},0);
+
+    //Display total spending on dashboard
+    document.querySelector("#total-expenses").textContent =`Rs.${total}`;
+    document.querySelector("#expense-count").textContent =expenses.length;
+
+    //when no expenses
+    if (expenses.length===0){
+        expenseList.innerHTML ="No expenses found.";
+        return;
+    }
+
+    //creating each expense to display
+    expenses.forEach(expense => {
+        const item=document.createElement("div");
+        item.className="expense-item";  //CSS class name
+
+        item.innerHTML=`
+        <h3>Amount:Rs.${Number(expense.amount)}</h3>
+        Description : ${expense.description},  
+        Category : ${expense.category},  
+        date : ${expense.date}
+        
+        <button class="edit-btn">EDIT</button>
+        <button class="delete-btn">DELETE</button>
+        `;  //These btn listeners are addedbelow
+
+        //Edit
+        const editbtn= item.querySelector(".edit-btn");
+        editbtn.addEventListener("click", function() {
+            amount.value = expense.amount;
+            description.value = expense.description;
+            category.value = expense.category;
+            date.value = expense.date
+
+            editingExpenseId= expense.id;
+            submitButton.textContent = "Update Expense";
+            window.scrollTo({top:0, behavior: "smooth"})
+        });
+
+        //Delete
+        const deletebtn= item.querySelector(".delete-btn");
+        deletebtn.addEventListener("click", function() {
+            const confirmed = confirm("Are you sure you want to delete this expense")
+
+            if (!confirmed) 
+                return;
+
+            fetch(`http://localhost:3000/api/expenses/${expense.id}`, {
+                method: "DELETE"
+            })
+
+            .then(response=>{
+                if (!response.ok) {
+                    throw new Error("Failed to delete this expense");
+                }
+                return response.json();
+            })
+
+            .then(data =>{
+                console.log(data);
+                loadExpenses();
+            })
+
+            .catch(error =>{
+                console.error(error);
+                showError("Could not delete expense");
+            });
+        });
+
+
+        expenseList.append(item); //This adds the created expense item into the HTML page
+    });
+
+};
+
+//Delete
+const deletebtn= document.querySelector(".delete-btn");
+
+
+//Loading
+function showLoading() {loadingMessage.style.display ="block";}
+function hideLoading() {loadingMessage.style.display ="none";}
+
+//Error
+function showError(message) {
+    errorMessage.textContent =message;
+    errorMessage.style.display = "block";
+}
+
+function hideError() {errorMessage.style.display ="none";}
+
+//Filters
+categoryFilter.addEventListener("change", loadExpenses);
+monthFilter.addEventListener("change", loadExpenses);
+
+//Initial load
+loadExpenses();
